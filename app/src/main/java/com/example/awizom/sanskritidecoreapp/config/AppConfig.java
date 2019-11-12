92444748c9e4c1baa293d08cc4cd44e7f11b8a99
Sakshidubey2708 <43462824+Sakshidubey2708@users.noreply.github.com>
2019-11-12 16:08:25 +0530

    final

--- app/src/main/java/com/example/awizom/sanskritidecoreapp/config/AppConfig.java
+++ app/src/main/java/com/example/awizom/sanskritidecoreapp/config/AppConfig.java
@@ -13,9 +13,9 @@ public class AppConfig {
 //      public static final String BASE_URL_API="http://192.168.1.23:8890/api/Admin/Admin/";
 //      public static final String BASE_URL_API_REG="http://192.168.1.23:8890/api/Account/";
 
-    public static final String BASE_URL="http://decore.jihuzzur.com/";
-    public static final String BASE_URL_API="http://decore.jihuzzur.com/api/Admin/Admin/";
-    public static final String BASE_URL_API_REG="http://decore.jihuzzur.com/api/Account/";
-    public static final String BASE_URL_API_USER="http://decore.jihuzzur.com/api/Admin/Admin/AllUserGet";
+    public static final String BASE_URL="http://decoreapp.jihuzzur.com/";
+    public static final String BASE_URL_API="http://decoreapp.jihuzzur.com/api/Admin/Admin/";
+    public static final String BASE_URL_API_REG="http://decoreapp.jihuzzur.com/api/Account/";
+    public static final String BASE_URL_API_USER="http://decoreapp.jihuzzur.com/api/Admin/Admin/AllUserGet";
 
 }
